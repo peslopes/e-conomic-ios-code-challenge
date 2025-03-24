ccd2ecd9f1f0039f1f043912458366642c24d821
Pedro Lopes <pedroesl96@gmail.com>
2025-03-24 22:40:48 +0000

    Update DataKit package

--- Packages/DataKit/Sources/DataKit/Receipts/Receipt.swift
+++ Packages/DataKit/Sources/DataKit/Receipts/Receipt.swift
@@ -6,4 +6,18 @@ public struct Receipt {
     public let date: Date
     public let amount: Double
     public let currency: String
+    
+    public init(
+        id: UUID = UUID(),
+        imageData: Data,
+        date: Date,
+        amount: Double,
+        currency: String
+    ) {
+        self.id = id
+        self.imageData = imageData
+        self.date = date
+        self.amount = amount
+        self.currency = currency
+    }
 }
